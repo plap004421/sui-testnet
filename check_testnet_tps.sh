46ddfd6be395137705801330041071463fdf055b
plap004421 <82771933+plap004421@users.noreply.github.com>
2023-02-22 22:49:55 +0300

    Create check_testnet_tps.sh

---
+++ check_testnet_tps.sh
@@ -0,0 +1,46 @@
+#CHECK TPS IN THE TESTNET.
+#FIRST ON LOCAL NODE, THEN ON A CLUSTER
+
+
+
+#!/bin/bash
+  
+if [ $(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt update
+  sudo apt install -y jq
+fi
+
+echo '-------------------------------------'
+echo $(date)
+echo 
+SUISTART=$(curl --location --request POST https://fullnode.testnet.sui.io:443 \
+--header 'Content-Type: application/json' \
+--data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
+
+NODESTART=$(curl --location --request POST 127.0.0.1:9000 \
+--header 'Content-Type: application/json' \
+--data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
+
+for I in {1..10}; do
+  sleep 1
+  BAR="$(yes . | head -n ${I} | tr -d '\n')"
+  printf "\rIN PROGRESS [%3d/100] %s" $((I * 10)) ${BAR}
+done
+
+printf "\n\n"
+
+SUIEND=$(curl --location --request POST https://fullnode.testnet.sui.io:443 \
+--header 'Content-Type: application/json' \
+--data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
+
+NODEEND=$(curl --location --request POST 127.0.0.1:9000 \
+--header 'Content-Type: application/json' \
+--data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
+
+SUITPS=$((($SUIEND-$SUISTART)/10))
+MYTPS=$((($NODEEND-$NODESTART)/10))
+
+echo 'SUI TPS: '$SUITPS
+echo 'NODE TPS: '$MYTPS
+echo '-------------------------------------'
