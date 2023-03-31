d2deb2e63e84e1d8e2bb351d67e6eeef4c836a69
plap004421 <82771933+plap004421@users.noreply.github.com>
2023-03-31 17:12:12 +0300

    Update sui-update.sh

--- sui-update.sh
+++ sui-update.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-SUIFOLDER = $1
-check = $2
+function=main
+C_LGn='\033[1;32m'
+C_R='\033[0;31m'
+RES='\033[0m'
 
-cd ${SUIFOLDER}
-git fetch upstream
-git checkout -B testnet --track upstream/testnet
+printf_n(){ printf "$1\n" "${@:2}"; }
 
-cargo build --release -p sui-node -p sui
-sui-node --version
+main(){
+  SUIFOLDER = $1
+  check = $2
 
-mv ${SUIFOLDER}/sui/target/release/sui-node /usr/local/bin/
-mv ${SUIFOLDER}/sui/target/release/sui /usr/local/bin/
+  cd ${SUIFOLDER}
+  git fetch upstream
+  git checkout -B testnet --track upstream/testnet
 
-sui-node --version
+  cargo build --release -p sui-node -p sui
+  sui-node --version
+  
+  mv ${SUIFOLDER}/sui/target/release/sui-node /usr/local/bin/
+  mv ${SUIFOLDER}/sui/target/release/sui /usr/local/bin/
+
+  sui-node --version
+  }
+  
+$function  
 
 #systemctl stop solana && systemctl restart suid.service && systemctl restart solana.service &&  journalctl -u suid -f
