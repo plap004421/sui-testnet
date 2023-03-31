57645be52ae77ef6d87b854ac1ea08b238be25c7
plap004421 <82771933+plap004421@users.noreply.github.com>
2023-03-31 16:27:35 +0300

    Update sui-update.sh

--- sui-update.sh
+++ sui-update.sh
@@ -6,6 +6,8 @@ cd ${SUIFOLDER}
 git checkout $2
 
 cargo build --release -p sui-node -p sui
+sui-node --version
+
 mv ${SUIFOLDER}/sui/target/release/sui-node /usr/local/bin/
 mv ${SUIFOLDER}/sui/target/release/sui /usr/local/bin/
 
