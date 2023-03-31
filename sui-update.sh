7b9bddb4f85d90f1f612f3655715ed59d3f54541
plap004421 <82771933+plap004421@users.noreply.github.com>
2023-03-31 17:45:53 +0300

    Update sui-update.sh

--- sui-update.sh
+++ sui-update.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-  cd /root/solana/sui/
-  git fetch upstream
-  git checkout -B testnet --track upstream/testnet
-
-  cargo build --release -p sui-node -p sui
-  sui-node --version
-  
-  mv target/release/sui-node /usr/local/bin/
-  mv target/release/sui /usr/local/bin/
-
-  sui-node --version
+cd /root/solana/sui/
+git fetch upstream
+git checkout -B testnet --track upstream/testnet
+cargo build --release -p sui-node -p sui
+sui-node --version
+mv target/release/sui-node /usr/local/bin/
+mv target/release/sui /usr/local/bin/
+sui-node --version
 
 #systemctl stop solana && systemctl restart suid.service && systemctl restart solana.service
